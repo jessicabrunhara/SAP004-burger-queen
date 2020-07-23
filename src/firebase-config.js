1af97a6b5e424794edeec31acfa3c4274ef67bbf
Daiane Vieira <daiane.vieira07@yahoo.com.br>
2020-07-22 22:02:13 -0300

    correção do arquivo firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,7 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
-const firebaseConfig = {
+const firebaseConfig = firebase.initializeApp({
     apiKey: "AIzaSyBfxDqhuXMgPiS8tQ6ZLBTyTsHS-SV9hSo",
     authDomain: "burger-queen-f1fd7.firebaseapp.com",
     databaseURL: "https://burger-queen-f1fd7.firebaseio.com",
@@ -7,6 +9,6 @@ const firebaseConfig = {
     storageBucket: "burger-queen-f1fd7.appspot.com",
     messagingSenderId: "789930118006",
     appId: "1:789930118006:web:785b80b6a37b3822a18857"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+});
+
+export default firebaseConfig;
