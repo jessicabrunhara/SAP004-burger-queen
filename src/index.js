54ce024ccda44185259c131206e8a65f545f2e25
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-07-24 15:55:36 -0300

    ajustes do config firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './global-style.css';
-import './config.js';
 
 ReactDOM.render(
     <React.StrictMode>
