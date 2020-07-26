7925228c26f391a6af0355da3c7c13cd27102f94
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-07-26 13:14:49 -0300

    inclusao options user

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -2,16 +2,38 @@ import React, { useState } from 'react';
 import './register.css';
 import firebase from 'Config/firebase';
 import 'firebase/auth';
+import 'firebase/firestore';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [msgType, setMsgType] = useState();
   const [msg, setMsg] = useState();
   const [loading, setLoading] = useState();
+  const [sector, setSector] = useState();
 
-  //const [name, setName] = useState();
+  /* function createUser(email, firstName, lastName, sector) {
+    console.log(sector.value);
+    if (sector.value === 'kitchen') {
+      firebase.firestore().collection('userKitchen').add({
+        firstName,
+        lastName,
+        email,
+        sector,
+        uid: firebase.auth().currentUser.uid,
+      });
+    } else {
+      firebase.firestore().collection('userLounge').add({
+        firstName,
+        lastName,
+        email,
+        uid: firebase.auth().currentUser.uid,
+      });
+    }
+  } */
 
   const signUp = () => {
 
@@ -25,9 +47,9 @@ const Register = () => {
     }
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(result => {
+      .then(() => {
         setLoading(0);
-        setMsgType('sucesso');
+        setMsgType('sucesso')
       }).catch(erro => {
         setLoading(0);
         setMsgType('erro')
@@ -51,23 +73,33 @@ const Register = () => {
     <div className='login-wrapper'>
       <div>Página de Registro</div>
       <form>
-        <input type='text' placeholder='Nome'></input>
+        <input onChange={(e) => setFirstName(e.target.value)} type='text' placeholder='Nome'></input>
+        <input onChange={(e) => setLastName(e.target.value)} type='text' placeholder='Sobrenome'></input>
         <input onChange={(e) => setEmail(e.target.value)} type='email' placeholder='email'></input>
         <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Senha'></input>
+
+        <div onChange={(e) => setSector(e.target.value)}>
+          <label>Selecione seu setor</label>
+          <label>Cozinha</label>
+          <input type='radio' value='kitchen' name='sector' id='kitchen' required />
+          <label>Salão</label>
+          <input type='radio' value='lounge' name='sector' id='lounge' required />
+        </div>
+
         {
-          loading ? <div class="spinner-border text-danger" role="status"><span class="sr-only">Loading...</span></div>
+          loading ? <div className="spinner-border text-danger" role="status"><span className="sr-only">Loading...</span></div>
             : <button onClick={signUp} type="button">Cadastrar</button>
         }
         <div>
           {msgType === 'sucesso' && <span><strong>WoW!</strong>Usuário cadastrado com sucesso! &#128526;</span>}
           {msgType === 'erro' && <span><strong>Ops!</strong> {msg} &#128546;</span>}
         </div>
-      </form>
+      </form >
       <div>
         <p>Já é cadastrado?</p>
         <Link to='/'>Faça login</Link>
       </div>
-    </div>
+    </div >
 
   )
 }
