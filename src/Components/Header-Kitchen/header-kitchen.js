2d675b3755fa6d6722ce3ad50097e617fc864846
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-08-02 19:42:10 -0300

    feito alterações de css em header

--- src/Components/Header-Kitchen/header-kitchen.js
+++ src/Components/Header-Kitchen/header-kitchen.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './header-kitchen.css';
 import LogoPiscando from 'Images/gifheader.gif';
 import { Link } from 'react-router-dom';
-import Button from 'Components/Button/button';
+import firebase from 'Config/firebase';
+import 'firebase/auth';
+
 
 const HeaderKitchen = () => {
 
@@ -23,19 +25,34 @@ const HeaderKitchen = () => {
             <div className='logo-wrapper'>
                 <img src={LogoPiscando} />
             </div>
-            <div className='btn-link-out'>
-                <button className='btn-out' onClick={signOut}><Link className='link' to='/'>Sair</Link></button>
-            </div>
+
+            {/* <button className='navbar-toggler' type='button' data-target='#navbar'>
+                <i className='fas fa-bars text-white'></i>
+                <div className='top-menu' id='navbar'>
+                    <ul className='navbar-nav mr-auto'>
+                        <li><Link className='link' to='/kitchen-new-order'>Pedido Cozinha</Link></li>
+                        <li><Link className='link' to='/kitchen-order-ready'>Pronto Cozinha</Link></li>
+                        <li><Link className='link' to='/finished-order'>Pedido Finalizado</Link></li>
+                        <li onClick={signOut}><Link className='link' to='/'>Sair</Link></li>
+                    </ul>
+                </div>
+            </button> */}
 
             <div className='all-btn-links'>
+
                 <div className='btn-routes'>
                     <button className='btn-links'><Link className='link' to='/kitchen-new-order'>Pedido Cozinha</Link></button>
                     <button className='btn-links'><Link className='link' to='/kitchen-order-ready'>Pronto Cozinha</Link></button>
                     <button className='btn-links'><Link className='link' to='/finished-order'>Pedido Finalizado</Link></button>
                 </div>
+
+                <div className='btn-link-out'>
+                    <button className='btn-out' onClick={signOut}><Link className='link' to='/'>Sair</Link></button>
+                </div>
+
             </div>
 
-        </header>
+        </header >
     );
 }
 
