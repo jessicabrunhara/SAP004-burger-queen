bf1775ad80bdaade6e1e67e4855cbc22062f30e7
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-08-05 19:08:50 -0300

    separacao de cafe da manha do all day

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './menu.css';
 import '../Lounge-Order-Ready/order-ready.css'
 import Button from 'Components/Button/button';
-import Input from 'Components/Input/input'
+import Input from 'Components/Input/input';
+import MyModal from 'Components/Modal/modal';
 
 const Menu = ({ items }) => {
 
@@ -12,17 +13,21 @@ const Menu = ({ items }) => {
         <h1>Menu</h1>
         <div className='all-menu'>
 
+
           <div className='menu-breakfast'>
             <div className='label'>Café da Manhã</div>
             <div className='btn-wrapper'>
-              {items.map((item, index) => <Button className='btn-menu' type='button' key={`btn-menu-${index}`}>{item.name}</Button>)}
+              {items.filter(item => item.breakfast === true).map((product, index) => <Button className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
             </div>
           </div>
 
           <div className='menu-lunch'>
             <div className='label'>Almoço/Jantar</div>
             <div className='btn-wrapper'>
-              <Button className='btn-menu' type='button' name='Hamburger Duplo' />
+              {items.filter(item => item.breakfast === false && item.hamburger === false).map((product, index) => <Button className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
+
+              {items.filter(item => item.breakfast === false && item.hamburger === true).map((product, index) => <MyModal className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </MyModal>)}
+
             </div>
           </div>
         </div >
