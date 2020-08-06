0de30f39d99850416aa2c80d554733d7caea4b22
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-08-06 11:34:46 -0300

    trazendo itens selecionados para o resumo de pedidos

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './menu.css';
 import '../Lounge-Order-Ready/order-ready.css'
 import Button from 'Components/Button/button';
@@ -7,24 +7,29 @@ import MyModal from 'Components/Modal/modal';
 
 const Menu = ({ items }) => {
 
+  const [menuItem, setMenuItem] = useState([]);
+
+  /* const orderTable = () => {
+     setMenuItem(menuItem + 1);
+   } */
+
   return (
     <div className='order-style'>
       <div className='menu-wrapper'>
         <h1>Menu</h1>
         <div className='all-menu'>
 
-
           <div className='menu-breakfast'>
             <div className='label'>Café da Manhã</div>
             <div className='btn-wrapper'>
-              {items.filter(item => item.breakfast === true).map((product, index) => <Button className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
+              {items.filter(item => item.breakfast === true).map((product, index) => <Button onClick={() => setMenuItem([...menuItem, product])} className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
             </div>
           </div>
 
           <div className='menu-lunch'>
             <div className='label'>Almoço/Jantar</div>
             <div className='btn-wrapper'>
-              {items.filter(item => item.breakfast === false && item.hamburger === false).map((product, index) => <Button className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
+              {items.filter(item => item.breakfast === false && item.hamburger === false).map((product, index) => <Button onClick={() => setMenuItem([...menuItem, product])} className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
 
               {items.filter(item => item.breakfast === false && item.hamburger === true).map((product, index) => <MyModal className='btn-menu' type='button' key={`btn-menu-${index}`} adds={product.adds} options={product.options} >{product.name} <br></br> R$ {product.price} </MyModal>)}
 
@@ -32,21 +37,20 @@ const Menu = ({ items }) => {
           </div>
         </div >
       </div>
+
       <div className='order-table-wrapper'>
         <div className='table-wrapper'>
           <Input className='input-style' placeholder='Mesa'>Mesa:</Input>
           <Input className='input-style' placeholder='Nome'>Cliente: </Input>
         </div>
+
         <div className='order-information-wrapper'>
           <div className='ordered-wrapper'>
-            <div className='quantify-ordered'>2 x</div>
-            <div className='item-ordered'>Batata Frita</div>
-          </div>
-          <div className='ordered-wrapper'>
-            <div className='quantify-ordered'>1 x</div>
-            <div className='item-ordered'>Hambúrger Duplo</div>
+            <div className='quantify-ordered'>{menuItem.map(product => <p>{product.name} {product.price}</p>)}</div>
+            <div className='item-ordered'></div>
           </div>
         </div>
+
         <div className='value-wrapper'>
           <div className='total-value'>Total</div>
           <div className='total-value'>R$5,00</div>
--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -7,9 +7,6 @@ import Input from 'Components/Input/input';
 const MyModal = (props) => {
     const [modalIsOpen, setModalIsOpen] = useState(false)
     console.log(props)
-    const teste = () => {
-        alert('deu certo')
-    }
 
     return (
         <div>
@@ -18,19 +15,34 @@ const MyModal = (props) => {
             <Modal {...props} isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className='modal-wrapper'>
                 <Button onClick={() => setModalIsOpen(false)} className='btn-modal-out'>X</Button>
                 <p>Escolha aqui suas opões: </p>
+                {props.options.map(option =>
+                    <>
+                        <Input type='radio' name='options' value={option}></Input>
+                        <label htmlFor={option}>{option}</label>
+                    </>
+                )}
 
-                <Input type='radio' name='options' value='carne'></Input>
-                <label for='carne'>{props.options[0]}</label>
-                <Input type='radio' name='options' value='frango'></Input>
-                <label for='frango'>{props.options[1]}</label>
-                <Input type='radio' name='options' value='vegetariano'></Input>
-                <label for='vegetariano'>{props.options[2]}</label>
+                {/* props.adds.map(item =>
+                    <>
+                        <Input type='checkbox' name={item}></Input>
+                        <label htmlFor={item}>{[adds]}</label>
+                    </>
+                )*/ }
 
-                <Input type='checkbox' name='egg'></Input>
-                <label for='egg'>Ovo {[props.adds].map(item => [item.egg])}</label>
+                {Object.entries(adds =>
+
+                    <>
+                        <Input type='checkbox' name={[]}></Input>
+                        <label htmlFor={[]}>{[adds].map(item => [item])}</label>
+                    </>
+                )}
+
+
+                {/* <Input type='checkbox' name='egg'></Input>
+                <label htmlFor='egg'>Ovo {[props.adds].map(item => [item.egg])}</label>
 
                 <Input type='checkbox' name='cheese'></Input>
-                <label for='cheese'>Queijo {[props.adds].map(item => [item.cheese])}</label>
+                <label htmlFor='cheese'>Queijo {[props.adds].map(item => [item.cheese])}</label> */}
 
             </Modal>
         </div>
