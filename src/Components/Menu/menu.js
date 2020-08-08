b95853ec322636929076bef2850eeaf68ebaf14c
Jessica Brunhara <brunharaj@gmail.com>
2020-08-08 15:06:05 -0300

    finalizada a adição dos itens

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -5,12 +5,32 @@ import Button from 'Components/Button/button';
 import Input from 'Components/Input/input';
 import MyModal from 'Components/Modal/modal';
 
-const Menu = ({ items, id }) => {
+const Menu = ({ items }) => {
 
   const [menuItem, setMenuItem] = useState([]);
-  const [quantify, setQuantify] = useState(1);
   const [table, setTable] = useState();
   const [client, setClient] = useState();
+  const [resume, setResume] = useState(0);
+
+  const changeQuantity = (product, change) => {
+    product.quantity += change
+
+    // Abaixo é importante usar "[...menuItem]" em vez de "menuItem"
+    // Se for usado apenas "menuItem", o React não vai perceber que houve uma mudança no array
+    // E, por isso, não vai recarregar o componente
+    setMenuItem([...menuItem])
+  }
+
+  const addNewItem = (product) => {
+    product.quantity = 1
+    setMenuItem([...menuItem, product])
+  }
+
+  // Descubra o valor total dos itens selecionados
+  let total = 0
+  menuItem.map(product => {
+    total += product.price * product.quantity
+  })
 
   return (
     <div className='order-style'>
@@ -21,14 +41,14 @@ const Menu = ({ items, id }) => {
           <div className='menu-breakfast'>
             <div className='label-menu'>Café da Manhã</div>
             <div className='btn-wrapper'>
-              {items.filter(item => item.breakfast === true).map((product, index) => <Button onClick={() => setMenuItem([...menuItem, product])} className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
+              {items.filter(item => item.breakfast === true).map((product, index) => <Button onClick={() => addNewItem(product)} className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
             </div>
           </div>
 
           <div className='menu-lunch'>
             <div className='label-menu'>Almoço / Jantar</div>
             <div className='btn-wrapper'>
-              {items.filter(item => item.breakfast === false && item.hamburger === false).map((product, index) => <Button onClick={() => setMenuItem([...menuItem, product])} className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
+              {items.filter(item => item.breakfast === false && item.hamburger === false).map((product, index) => <Button onClick={() => addNewItem(product)} className='btn-menu' type='button' key={`btn-menu-${index}`}>{product.name} <br></br> R$ {product.price} </Button>)}
 
               {items.filter(item => item.breakfast === false && item.hamburger === true).map((product, index) => <MyModal className='btn-menu' type='button' key={`btn-menu-${index}`} adds={product.adds} options={product.options} >{product.name} <br></br> R$ {product.price} </MyModal>)}
 
@@ -47,13 +67,13 @@ const Menu = ({ items, id }) => {
           <div className='ordered-wrapper'>
             {menuItem.map(product =>
               <div className='item-ordered'>
-                <p >{product.name}</p>
+                <p>{product.name}</p>
                 <div className='btn-order-wrapper'>
-                  <Button className='btn-add' onClick={() => setQuantify(quantify + 1)}>+</Button>
-                  <div className='quantify-ordered'>{quantify}</div>
-                  <Button className='btn-add' onClick={() => setQuantify(quantify - 1)}>-</Button>
+                  <Button className='btn-add' onClick={() => changeQuantity(product, 1)}>+</Button>
+                  <div className='quantify-ordered'>{product.quantity}</div>
+                  <Button className='btn-add' onClick={() => changeQuantity(product, -1)}>-</Button>
                 </div>
-                <div className='price-ordered'>{product.price}</div>
+                <div className='price-ordered'>{product.quantity * product.price}</div>
               </div>
             )}
           </div>
@@ -61,7 +81,7 @@ const Menu = ({ items, id }) => {
 
         <div className='value-wrapper'>
           <div className='total-value'>Total</div>
-          <div className='total-value'>R$5,00</div>
+          <div className='total-value'>{total}</div>
         </div>
         <div className='btn-wrapper'>
           <Button className='btn-std'> Enviar</Button>
