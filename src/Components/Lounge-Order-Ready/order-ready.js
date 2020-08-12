bdb2891fe47b19616bb5cb8200d3c251b11c02bd
Jessica Brunhara <brunharaj@gmail.com>
2020-08-12 19:05:25 -0300

    ajustes css modal

--- src/Components/Lounge-Order-Ready/order-ready.js
+++ src/Components/Lounge-Order-Ready/order-ready.js
@@ -3,30 +3,30 @@ import 'Components/Lounge-Order-Ready/order-ready.css';
 import Button from 'Components/Button/button';
 
 const OrderReady = () => {
-	return (
-		<div className='order-ready-wrapper'>
-			<div className='table-wrapper'>
-				<div className='time-wrapper'>10m</div>
-				<div className='table-number'>Mesa:</div>
-				<div className='client-information'>Cliente: </div>
-				<div className='waiter-information'>Atendente: </div>
-			</div>
-			<div className='order-information-wrapper'>
-				<div className='ordered-wrapper'>
-					<div className='quantify-ordered'>2 x</div>
-					<div className='item-ordered'>Batata Frita</div>
-				</div>
-				<div className='ordered-wrapper'>
-					<div className='quantify-ordered'>1 x</div>
-					<div className='item-ordered'>Hambúrger Duplo</div>
-				</div>
-			</div>
-			<div className='btn-wrapper'>
-				<Button className='btn-std'> Entregar</Button>
-			</div>
+  return (
+    <div className='order-ready-wrapper'>
+      <div className='table-wrapper'>
+        <div className='time-wrapper'>10m</div>
+        <div className='table-number'>Mesa:</div>
+        <div className='client-information'>Cliente: </div>
+        <div className='waiter-information'>Atendente: </div>
+      </div>
+      <div className='order-information-wrapper'>
+        <div className='ordered-wrapper'>
+          <div className='quantify-ordered'>2 x</div>
+          <div className='item-ordered'>Batata Frita</div>
+        </div>
+        <div className='ordered-wrapper'>
+          <div className='quantify-ordered'>1 x</div>
+          <div className='item-ordered'>Hambúrger Duplo</div>
+        </div>
+      </div>
+      <div className='btn-wrapper'>
+        <Button className='btn-std'> Entregar</Button>
+      </div>
 
-		</div>
-	)
+    </div>
+  )
 
 }
 
--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -5,61 +5,59 @@ import './modal.css';
 import Input from 'Components/Input/input';
 
 const MyModal = (props) => {
-    const [modalIsOpen, setModalIsOpen] = useState(false)
-    const [burgerOption, setBurgerOption] = useState('')
-    const [adds, setAdds] = useState([])
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [burgerOption, setBurgerOption] = useState('')
+  const [adds, setAdds] = useState([])
 
-    const onClickAdd = (e) => {
-        const selectedAdd = e.target.value
-        if (e.target.checked) {
-            setAdds([...adds, selectedAdd])
-        } else {
-            console.log('qualquer coisa')
-            setAdds([selectedAdd])
+  const onClickAdd = (e) => {
+    const selectedAdd = e.target.value
+    if (e.target.checked) {
+      setAdds([...adds, selectedAdd])
+    } else {
+      console.log('qualquer coisa')
+      setAdds([selectedAdd])
 
-        }
     }
-    const sendAdds = () => {
-        console.log(props.product.price)
-        console.log(adds)
-        props.addNewItem({ ...props.product, adds, burgerOption, price: props.product.price + adds.length })
-        console.log('adiciona aí')
-    }
+  }
+  const sendAdds = () => {
+    console.log(props.product.price)
+    console.log(adds)
+    props.addNewItem({ ...props.product, adds, burgerOption, price: props.product.price + adds.length })
+    console.log('adiciona aí')
+  }
 
-    return (
-        <div>
-            <Button onClick={() => setModalIsOpen(true)} className='btn-menu'>{props.children}</Button>
+  return (
+    <div>
+      <Button onClick={() => setModalIsOpen(true)} className='btn-menu'>{props.children}</Button>
 
-            <Modal {...props} isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className='modal-wrapper'>
-                <Button onClick={() => setModalIsOpen(false)} className='btn-modal-out'><p className='modal-close'>X</p></Button>
-                <div className='modal-options'>Opções do Hambúrguer</div>
-                {props.options.map(option =>
-                    <>
-                        <span><Input type='radio' onChange={(e) => {
-                            setBurgerOption(e.target.value)
-                            console.log(e.target.value)
-                        }} className='radio-modal' name='options' value={option} />
-                            <aria-label htmlFor={option}>{option}</aria-label></span>
-                    </>
-                )}
-                <div className='modal-options'>Adicionais: </div>
-
-                {/* <span>
-                     <Input type='checkbox'></Input>
-                    <aria-label className='radio-label'> Ovo </aria-label></span> */}
-
-                {Object.entries(props.adds).map(item =>
-                    <>
-                        <Input type='checkbox' value={item[0]} onChange={onClickAdd} />
-                        <label htmlFor={[]}>{item[0]} {item[1]}</label>
-                    </>
-                )}
-
-                <Button onClick={sendAdds} type='button'>Adicionar</Button>
-
-            </Modal>
+      <Modal {...props} isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className='modal-wrapper'>
+        <Button onClick={() => setModalIsOpen(false)} className='btn-modal-out'><p className='modal-close'>X</p></Button>
+        <div className='modal-options'>Opções do Hambúrguer</div>
+        {props.options.map(option =>
+          <>
+            <span><Input type='radio' onChange={(e) => {
+              setBurgerOption(e.target.value)
+              console.log(e.target.value)
+            }} className='radio-modal' name='options' value={option} />
+              <aria-label htmlFor={option}>{option}</aria-label></span>
+          </>
+        )}
+        <div className='adds-wrapper'>
+          <div className='modal-options'>Adicionais: </div>
+          <div className='input-radio-wrapper'>
+            {Object.entries(props.adds).map(item =>
+              <div className='input-radio'>
+                <Input type='checkbox' value={item[0]} onChange={onClickAdd} />
+                <label className='input-radio-label' htmlFor={[]}>{item[0]} R${item[1]},00</label>
+              </div>
+            )}
+          </div>
         </div>
-    );
+        <Button onClick={sendAdds} className='send-btn-modal' type='button'>Adicionar</Button>
+
+      </Modal>
+    </div>
+  );
 };
 
 export default MyModal;
